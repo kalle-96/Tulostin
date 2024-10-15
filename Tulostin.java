7c5d2713bf9389aa38685f411eaa2017c80dfca2
Kalle Saarinen <kallemikael96@gmail.com>
2024-10-15 13:48:45 +0300

    Muuttujien luonti

---
+++ Tulostin.java
@@ -0,0 +1,20 @@
+public class Tulostin {
+    public static void main(String[] args) {
+        
+        String tekija = "Kalle"; 
+
+        
+        double luku1 = 5;
+        double luku2 = 2;
+
+        
+        double tulo;
+        double erotus;
+        double summa;
+        double jako;
+
+        
+    }
+
+}
+
